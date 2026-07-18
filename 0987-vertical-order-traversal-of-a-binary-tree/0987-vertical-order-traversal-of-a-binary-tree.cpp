139c73c67c6e39b3857d8b1c268f4cec37d475e7
Pranjal Raj <pranjalraj0404@gmail.com>
2026-07-18 21:24:19 +0530

    Time: 3 ms (40.38%), Space: 15.8 MB (71.75%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -8,43 +8,38 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */class Solution {
+ */
+class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
 
         map<int, map<int, multiset<int>>> nodes;
 
         queue<pair<TreeNode*, pair<int,int>>> q;
-
         q.push({root,{0,0}});
 
         while(!q.empty()){
-
-            auto cur=q.front();
+            auto it = q.front();
             q.pop();
 
-            TreeNode* node=cur.first;
-            int x=cur.second.first;
-            int y=cur.second.second;
+            TreeNode* cur = it.first;
+            int x = it.second.first;
+            int y = it.second.second;
 
-            nodes[x][y].insert(node->val);
+            nodes[x][y].insert(cur->val);
 
-            if(node->left)
-                q.push({node->left,{x-1,y+1}});
+            if(cur->left)
+                q.push({cur->left,{x-1,y+1}});
 
-            if(node->right)
-                q.push({node->right,{x+1,y+1}});
+            if(cur->right)
+                q.push({cur->right,{x+1,y+1}});
         }
 
         vector<vector<int>> ans;
-
-        for(auto &col:nodes){
-
+        for(auto &col : nodes){
             vector<int> temp;
-
-            for(auto &row:col.second){
-
-                for(int val:row.second)
+            for(auto &row : col.second){
+                for(int val : row.second)
                     temp.push_back(val);
             }
 
