4a0274f575ee168df5163b99323e35a93fbf0913
Pranjal Raj <pranjalraj0404@gmail.com>
2026-08-16 23:56:40 +0530

    Time: 0 ms (100%), Space: 19.2 MB (68.22%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool dfs(int node , vector<vector<int>> &adj, vector<int> &vis){
+        if (vis[node] == 1)
+            return true;
+        if (vis[node] == 2)
+            return false;
+        vis[node] = 1;
+        for (int adjNode : adj[node]) {
+            if (dfs(adjNode, adj, vis))
+                return true;
+        }
+        vis[node] = 2;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for (auto &p : prerequisites) {
+            adj[p[1]].push_back(p[0]);
+        }
+        vector<int> vis(numCourses, 0);
+        for (int i = 0; i < numCourses; i++) {
+            if (dfs(i, adj, vis))
+                return false;
+        }
+        return true;
+    }
+};
